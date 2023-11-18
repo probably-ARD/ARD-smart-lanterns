4936e49d36886c281994938f0dcd81ae3c334308
probably-ARD <durakov.sasha95@mail.ru>
2023-11-18 22:08:37 +0300

    new sketch, README

---
+++ README.md
@@ -0,0 +1,11 @@
+# ARD-smart-lanterns
+
+Программа для макета системы умных автострадных фонарей. 
+Работает просто. Если расстояние сейчас сильно меньше предыдущего, включается светодиод.
+Разработано на основе ардуино уно.
+
+A program for the layout of a system of smart motorway lights. 
+It works simply. If the distance is now much less than the previous one, the LED turns on.
+Developed on the basis of Arduino uno.
+
+![Схема подключения / Сonnection scheme](/materials/scheme.jpg)
--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,5 +1,3 @@
-#include <NewPing.h>
-
 //
 //░█████╗░██████╗░██████╗░  ██████╗░██████╗░░█████╗░░░░░░██╗███████╗░█████╗░████████╗
 //██╔══██╗██╔══██╗██╔══██╗  ██╔══██╗██╔══██╗██╔══██╗░░░░░██║██╔════╝██╔══██╗╚══██╔══╝
@@ -10,111 +8,64 @@
 //
 
 String author = "probably-ARD";
-String version = "1.0.1";
+String version = "1.2.4";
 
-// стартовые настройки =======================================================================
+// sketch settings ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-// 1 сонар
-#define TRIG_1 12 // D port
-#define ECHO_1 11 // D port
-#define LAMP_1 10 // D port
-#define MAX_DISTANCE_1 200 // cm
+// sonars trig and echo pins (!!! D ports !!!)
+#define TRIG_1 3
+#define TRIG_2 6
+#define TRIG_3 13
 
-// 2 сонар
-#define TRIG_2 9 // D port
-#define ECHO_2 8 // D port
-#define LAMP_2 7 // D port
-#define MAX_DISTANCE_2 200 // cm
+#define ECHO_1 2
+#define ECHO_2 5
+#define ECHO_3 12
 
-// 3 сонар
-#define TRIG_3 6 // D port
-#define ECHO_3 5 // D port
-#define LAMP_3 4 // D port
-#define MAX_DISTANCE_3 200 // cm
+// lamps pins (!!! D ports !!!)
+#define LAMP_1 7
+#define LAMP_2 10
+#define LAMP_3 4
 
-// 1 кнопка
-#define BTN_1 1 // A port
-
-// 2 кнопка
-#define BTN_2 2 // A port
-
-// 3 кнопка
-#define BTN_3 3 // A port
-
-// время горения света в мс
+// lamps times in ms
 #define LIGHT_TIME 15000
 
-// ===========================================================================================
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-// переменные --------------------------------------------------------------------------------
+// variables ------------------------------------------------------------------------
 
-// сонары
-NewPing sonar_1(TRIG_1, TRIG_1, MAX_DISTANCE_1);
-NewPing sonar_2(TRIG_2, TRIG_2, MAX_DISTANCE_2);
-NewPing sonar_3(TRIG_3, TRIG_3, MAX_DISTANCE_3);
+int duration, distance;
+int old_distance_1, old_distance_2, old_distance_3;
+int now_distance_1, now_distance_2, now_distance_3;
+long int start_lamp_1, start_lamp_2, start_lamp_3;
+bool move_1, move_2, move_3;
 
-// длина
-unsigned int distance_1;
-unsigned int distance_2;
-unsigned int distance_3;
+// ----------------------------------------------------------------------------------
 
-// предыдущие значения
-int past_distances_1;
-int past_distances_2;
-int past_distances_3;
+// funcs ============================================================================
 
-// наличие движения нужны чтобы не было постоянного переключения тока
-bool move_1 = false;
-bool move_2 = false;
-bool move_3 = false;
-
-// начало горения лампы
-long int start_lamp_1;
-long int start_lamp_2;
-long int start_lamp_3;
-
-// -------------------------------------------------------------------------------------------
-
-void setup() {
-  Serial.begin(9600);
+int getDistance(int trig, int echo){
+  // zeroing out hc-sr04
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
   
-  // первоначальное чтение датчиков
-  distance_1 = sonar_1.ping_cm();
-  distance_2 = sonar_2.ping_cm();
-  distance_3 = sonar_3.ping_cm();
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
 
-  past_distances_1 = distance_1;
-  past_distances_2 = distance_2;
-  past_distances_3 = distance_3;
+  digitalWrite(trig, LOW);
+  
+  // get hc-sr04 duration
+  duration = pulseIn(echo, HIGH);
+  distance = duration / 58.8;
+
+  return distance;
 }
 
-void loop() {
-  delay(200);
-  // дистанция с датчиков
-  distance_1 = sonar_1.ping_cm();
-  distance_2 = sonar_2.ping_cm();
-  distance_3 = sonar_3.ping_cm();
-
-  // чтение кнопок, на случай если датчики плохо работают ====================================
-  if (analogRead(BTN_1) == 1023){
-    distance_1 = (past_distances_1 + 10) * 10;
-    Serial.println("ДВИЖЕНИЕ С КНОПКИ 1");
-  };
-  if (analogRead(BTN_2) == 1023){
-    distance_2 = (past_distances_2 + 10) * 10;
-    Serial.println("ДВИЖЕНИЕ С КНОПКИ 2");
-  };
-  if (analogRead(BTN_3) == 1023){
-    distance_3 = (past_distances_3 + 10) * 10;
-    Serial.println("ДВИЖЕНИЕ С КНОПКИ 3");
-  };
-  // ==========================================================================================
-
-  // определяем движение и включаем свет ------------------------------------------------------
-
-  if (distance_1 > (past_distances_1 * 2)){
+void lampStartStop(){
+  
+  // on lamps if move true
+  if ((now_distance_1 * 1.5) < old_distance_1){
     move_1 = true;
-    Serial.println("ДВИЖЕНИЕ НА 1 ДАТЧИКЕ");
+    Serial.println("move on 1 sonar");
     
     digitalWrite(LAMP_1, HIGH);
     start_lamp_1 = millis();
@@ -122,11 +73,12 @@ void loop() {
     digitalWrite(LAMP_2, HIGH);
     start_lamp_2 = millis();
     
-    Serial.println("ВКЛЮЧАЮ 1 И 2 ЛАМПЫ");
+    Serial.println("turn ON 1, 2 lamp");
   }
-  if (distance_2 > (past_distances_2 * 2)){
+  
+  if ((now_distance_2 * 1.5) < old_distance_2){
     move_2 = true;
-    Serial.println("ДВИЖЕНИЕ НА 2 ДАТЧИКЕ");
+    Serial.println("move on 2 sonar");
     
     digitalWrite(LAMP_1, HIGH);
     start_lamp_1 = millis();
@@ -137,11 +89,12 @@ void loop() {
     digitalWrite(LAMP_3, HIGH);
     start_lamp_3 = millis();
     
-    Serial.println("ВКЛЮЧАЮ 1 И 2 И 3 ЛАМПЫ");
+    Serial.println("turn ON 1, 2, 3 lamp");
   }
-  if (distance_3 > (past_distances_3 * 2)){
+  
+  if ((now_distance_3 * 1.5) < old_distance_3){
     move_3 = true;
-    Serial.println("ДВИЖЕНИЕ НА 3 ДАТЧИКЕ");
+    Serial.println("move on 3 sonar");
     
     digitalWrite(LAMP_2, HIGH);
     start_lamp_2 = millis();
@@ -149,36 +102,53 @@ void loop() {
     digitalWrite(LAMP_3, HIGH);
     start_lamp_3 = millis();
     
-    Serial.println("ВКЛЮЧАЮ 2 И 3 ЛАМПЫ");
+    Serial.println("turn ON 2, 3 lamp");
   }
 
-  // ------------------------------------------------------------------------------------------
-
-  // выключаем свет, если горит больше заданного времени ======================================
-
+  // off lamps if time skeep
   if ((move_1 == true) or (move_2 == true) or (move_3 == true)){
+    
     if (millis() - start_lamp_1 > LIGHT_TIME){
       move_1 = false;
       digitalWrite(LAMP_1, LOW);
-      Serial.println("ВЫКЛЮЧАЮ 1 ЛАМПУ");
+      Serial.println("turn OFF 1 lamp");
     }
     
     if (millis() - start_lamp_2 > LIGHT_TIME){
       move_2 = false;
       digitalWrite(LAMP_2, LOW);
-      Serial.println("ВЫКЛЮЧАЮ 2 ЛАМПУ");
+      Serial.println("turn OFF 2 lamp");
     }
 
     if (millis() - start_lamp_3 > LIGHT_TIME){
       move_3 = false;
       digitalWrite(LAMP_3, LOW);
-      Serial.println("ВЫКЛЮЧАЮ 3 ЛАМПУ");
+      Serial.println("turn OFF 3 lamp");
     }
   }
-  // ==========================================================================================
+}
 
-  // переписываем старые данные
-  past_distances_1 = distance_1;
-  past_distances_2 = distance_2;
-  past_distances_3 = distance_3;
+// ==================================================================================
+
+void setup() {
+  Serial.begin(9600);
+  
+  // filling start distances
+  old_distance_1 = getDistance(TRIG_1, ECHO_1);
+  old_distance_2 = getDistance(TRIG_2, ECHO_2);
+  old_distance_3 = getDistance(TRIG_3, ECHO_3);
+}
+
+void loop() {
+  now_distance_1 = getDistance(TRIG_1, ECHO_1);
+  now_distance_2 = getDistance(TRIG_2, ECHO_2);
+  now_distance_3 = getDistance(TRIG_3, ECHO_3);
+
+  lampStartStop();
+
+  old_distance_1 = now_distance_1;
+  old_distance_2 = now_distance_2;
+  old_distance_3 = now_distance_3;
+
+  delay(50);
 }
